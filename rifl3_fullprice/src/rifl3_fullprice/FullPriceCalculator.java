026008fb809dc4ea317db3baad2b600efe42f966
Tamas Borbas <borbastomi@gmail.com>
2015-03-11 19:58:05 +0100

    orders matching problem resolved

--- rifl3_fullprice/src/rifl3_fullprice/FullPriceCalculator.java
+++ rifl3_fullprice/src/rifl3_fullprice/FullPriceCalculator.java
@@ -3,11 +3,14 @@ package rifl3_fullprice;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.QueueingConsumer;
+import com.rabbitmq.client.QueueingConsumer.Delivery;
 
 import datamodel.DeliveryData;
 import datamodel.Order;
@@ -22,6 +25,8 @@ public class FullPriceCalculator implements Runnable {
 	private QueueingConsumer deliveryConsumer;
 	private static final String IN_QUEUE_PRICE_NAME = "net";
 	private static final String IN_QUEUE_DELIV_NAME = "delivery";
+	private List<QueueingConsumer.Delivery> delivOrders;
+	private List<QueueingConsumer.Delivery> priceOrders;
 
 	public FullPriceCalculator(String brokerIP) {
 		try {
@@ -32,17 +37,18 @@ public class FullPriceCalculator implements Runnable {
 
 			channel.queueDeclare(IN_QUEUE_PRICE_NAME, false, false, false, null);
 			channel.queueDeclare(IN_QUEUE_DELIV_NAME, false, false, false, null);
-			System.out
-					.println(" [*] Waiting for messages. To exit press CTRL+C");
 
 			priceConsumer = new QueueingConsumer(channel);
-			channel.basicConsume(IN_QUEUE_PRICE_NAME, true, priceConsumer);
+			boolean autoAck = false;
+			channel.basicConsume(IN_QUEUE_PRICE_NAME, autoAck, priceConsumer);
 			deliveryConsumer = new QueueingConsumer(channel);
-			channel.basicConsume(IN_QUEUE_DELIV_NAME, true, deliveryConsumer);
-
+			channel.basicConsume(IN_QUEUE_DELIV_NAME, autoAck, deliveryConsumer);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+
+		priceOrders = new ArrayList<QueueingConsumer.Delivery>();
+		delivOrders = new ArrayList<QueueingConsumer.Delivery>();
 	}
 
 	private void calculateFullPrice(DeliveryData deliveryData,
@@ -59,27 +65,57 @@ public class FullPriceCalculator implements Runnable {
 	public void run() {
 		while (!exit) {
 			if (isrunning) {
-				Order delivorder;
-				Order priceorder;
 				try {
-					QueueingConsumer.Delivery price = priceConsumer
-							.nextDelivery();
-					priceorder = deserializeOrder(price.getBody());
-					QueueingConsumer.Delivery delivery = deliveryConsumer
-							.nextDelivery();
-					delivorder = deserializeOrder(delivery.getBody());
-
-					System.out.println("BEFORE CALC"+priceorder.toString());
-					calculateFullPrice(delivorder.getDeliveryData(),
-							priceorder.getPriceData());
-					System.out.println("AFTER CALC"+priceorder.toString());
-					isrunning = false;
+					QueueingConsumer.Delivery price = priceConsumer.nextDelivery(100);
+					if(price!=null)
+						priceOrders.add(price);
 				} catch (InterruptedException e) {
 					e.printStackTrace();
-				} catch (ClassNotFoundException e) {
+				}
+				
+				try {
+					QueueingConsumer.Delivery delivery = deliveryConsumer.nextDelivery(100);
+					if(delivery!=null)
+						delivOrders.add(delivery);
+				} catch (InterruptedException e) {
 					e.printStackTrace();
-				} catch (IOException e) {
-					e.printStackTrace();
+				}
+				if(delivOrders.size()>0) {
+					int priceIndex = -1;
+					int deliveryIndex = -1;
+					for (Delivery priceOrderD : priceOrders) {
+						for (Delivery deliveryOrderD : delivOrders) {
+							try {
+								Order priceOrder = deserializeOrder(priceOrderD.getBody());
+								Order deliveryOrder = deserializeOrder(deliveryOrderD.getBody());
+								
+								if(priceOrder.getId()==deliveryOrder.getId() && isrunning) {
+									priceIndex = priceOrders.indexOf(priceOrderD);
+									deliveryIndex = delivOrders.indexOf(deliveryOrderD);
+									try {
+										System.out.println("BEFORE CALC\n"+priceOrder.toString());
+										calculateFullPrice(deliveryOrder.getDeliveryData(),
+												priceOrder.getPriceData());
+										System.out.println("AFTER CALC\n"+priceOrder.toString());
+										
+										channel.basicAck(priceOrderD.getEnvelope().getDeliveryTag(), false);
+										channel.basicAck(deliveryOrderD.getEnvelope().getDeliveryTag(), false);
+										
+										isrunning = false;
+									} catch (InterruptedException e) {
+										e.printStackTrace();
+									}
+								}
+							} catch (ClassNotFoundException | IOException e1) {
+								e1.printStackTrace();
+							}
+						}
+					}
+					if(priceIndex>-1 && deliveryIndex>-1 && !isrunning) {
+						System.out.println("Price order enqueued: "+((priceOrders.remove(priceIndex)!=null)?"OK":"NOT"));
+						System.out.println("Delivery order enqueued: "+((delivOrders.remove(deliveryIndex)!=null)?"OK":"NOT"));
+						
+					}
 				}
 			} else {
 				try {
@@ -106,6 +142,7 @@ public class FullPriceCalculator implements Runnable {
 		try {
 			channel.close();
 			connection.close();
+			System.out.println("Connections cloased successfully.");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
