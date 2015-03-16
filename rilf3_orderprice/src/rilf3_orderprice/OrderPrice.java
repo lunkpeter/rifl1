e1b4073fad99457081fed7c2341fa9a7fc49949b
Tamas Borbas <borbastomi@gmail.com>
2015-03-16 08:16:08 +0100

    local ip print to console (for demonstrate separate run).

--- rilf3_orderprice/src/rilf3_orderprice/OrderPrice.java
+++ rilf3_orderprice/src/rilf3_orderprice/OrderPrice.java
@@ -3,13 +3,17 @@ package rilf3_orderprice;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.UnknownHostException;
+import java.util.Enumeration;
 
 public class OrderPrice {
 	public static void main(String[] args) {
 		boolean exit = false;
 		String brokerIP="localhost";
 		if(args.length>0) {
-			brokerIP=args[1];
+			brokerIP=args[0];
 		}
 		
 		OrderPriceCalculator calc = new OrderPriceCalculator(brokerIP);
@@ -17,6 +21,11 @@ public class OrderPrice {
 		mythread.start();
 		
 		System.out.println("AMQP connection established at: "+brokerIP);
+		try {
+			System.out.println("Local IP: "+getLocalHostLANAddress().getHostAddress());
+		} catch (Exception ex) {
+			
+		}
 		System.out.println("Type \"step\" to step the workflow");
 		System.out.println("Type \"quit\" to exit");
 		while(!exit)
@@ -41,5 +50,52 @@ public class OrderPrice {
 		}
 		
 		calc.exit = true;
+	}
+	
+	private static InetAddress getLocalHostLANAddress() throws UnknownHostException {
+	    try {
+	        InetAddress candidateAddress = null;
+	        // Iterate all NICs (network interface cards)...
+	        for (Enumeration<NetworkInterface> ifaces = NetworkInterface.getNetworkInterfaces(); ifaces.hasMoreElements();) {
+	            NetworkInterface iface = (NetworkInterface) ifaces.nextElement();
+	            // Iterate all IP addresses assigned to each card...
+	            for (Enumeration<InetAddress> inetAddrs = iface.getInetAddresses(); inetAddrs.hasMoreElements();) {
+	                InetAddress inetAddr = (InetAddress) inetAddrs.nextElement();
+	                if (!inetAddr.isLoopbackAddress()) {
+
+	                    if (inetAddr.isSiteLocalAddress()) {
+	                        // Found non-loopback site-local address. Return it immediately...
+	                        return inetAddr;
+	                    }
+	                    else if (candidateAddress == null) {
+	                        // Found non-loopback address, but not necessarily site-local.
+	                        // Store it as a candidate to be returned if site-local address is not subsequently found...
+	                        candidateAddress = inetAddr;
+	                        // Note that we don't repeatedly assign non-loopback non-site-local addresses as candidates,
+	                        // only the first. For subsequent iterations, candidate will be non-null.
+	                    }
+	                }
+	            }
+	        }
+	        if (candidateAddress != null) {
+	            // We did not find a site-local address, but we found some other non-loopback address.
+	            // Server might have a non-site-local address assigned to its NIC (or it might be running
+	            // IPv6 which deprecates the "site-local" concept).
+	            // Return this non-loopback candidate address...
+	            return candidateAddress;
+	        }
+	        // At this point, we did not find a non-loopback address.
+	        // Fall back to returning whatever InetAddress.getLocalHost() returns...
+	        InetAddress jdkSuppliedAddress = InetAddress.getLocalHost();
+	        if (jdkSuppliedAddress == null) {
+	            throw new UnknownHostException("The JDK InetAddress.getLocalHost() method unexpectedly returned null.");
+	        }
+	        return jdkSuppliedAddress;
+	    }
+	    catch (Exception e) {
+	        UnknownHostException unknownHostException = new UnknownHostException("Failed to determine LAN address: " + e);
+	        unknownHostException.initCause(e);
+	        throw unknownHostException;
+	    }
 	}
 }
